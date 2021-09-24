a7e67792c96178a19ff005470e2c22654201ae75
Eric Chen <eschen@cpp.edu>
2021-09-23 19:14:41 -0700

    Outline for both programs added

---
+++ numberGuessingGame.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Ask for user input 1 , 2 , or 3
+
+/* Option 1: Ask for user input for number 1-10
+If input == random number then user wins and returns to menu
+If input != random number then tell if too high or low and prompt for guess
+If q input then game should end and return to menu  */
+
+//Option 2: Prompt input for max value and write to file. Set max value and ensure they do not exceed it.
+
+//Option 3: Thank user for playing and end game.
---
+++ restaurantBill.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Create array with meal cost of all 4 items
+
+//Randomly picks 1 item from meal array and displays item and cost
+
+//Accept user input for tax %, calculates, and displays tax amount
+
+//Accept user input for tip %, calculates, and displays tip amount
+
+//Adds meal cost, tax, and tip and displays total bill
